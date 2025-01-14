e4f18d7aba11c7f523a36c654c9a35877cea565e
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-01-14 21:48:43 +0200

    Update d-flux.js

--- plugins/d-flux.js
+++ plugins/d-flux.js
@@ -2,49 +2,49 @@ const config = require('../config');
 const { cmd, commands } = require('../command');
 const { fetchJson } = require('../lib/functions');
 
+
 cmd({
-pattern: 'flux3',
-desc: 'Generate an image using Flux',
-category: 'image',
-filename: __filename
+  pattern: 'flux3',
+  desc: 'Generate an image using Flux',
+  category: 'image',
+  filename: __filename
 }, async (conn, mek, m, {
-text,
-from,
-quoted,
-body,
-isCmd,
-command,
-args,
-q,
-isGroup,
-sender,
-senderNumber,
-botNumber2,
-botNumber,
-pushname,
-isMe,
-isOwner,
-groupMetadata,
-groupName,
-participants,
-groupAdmins,
-isBotAdmins,
-isAdmins,
-reply
+  text,
+  from,
+  quoted,
+  body,
+  isCmd,
+  command,
+  args,
+  q,
+  isGroup,
+  sender,
+  senderNumber,
+  botNumber2,
+  botNumber,
+  pushname,
+  isMe,
+  isOwner,
+  groupMetadata,
+  groupName,
+  participants,
+  groupAdmins,
+  isBotAdmins,
+  isAdmins,
+  reply
 }) => {
-try {
-if (!text) {
-return reply(`*Usage:* ${command} <prompt>\n\n*Example:* ${command} cat`);
-}
+  try {
+    if (!text) {
+      return reply(`*Usage:* ${command} <prompt>\n\n*Example:* ${command} cat`);
+    }
 
-await reply('> *Processing...*');
+    await reply('> *Processing...*');
 
-const apiUrl = `https://api.davidcyriltech.my.id/flux?prompt=${encodeURIComponent(text)}`;
+    const apiUrl = `https://api.davidcyriltech.my.id/flux?prompt=${encodeURIComponent(text)}`;
 
-await conn.sendMessage(m.chat, { image: { url: apiUrl }, caption: `🎨 *Flux Image Generator*\n\n📄 *Prompt:* ${text}\n\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀᴠɪᴅ ᴄʏʀɪʟ ᴛᴇᴄʜ`, }, { quoted: m });
-
-} catch (error) {
-console.error('Error in Flux command:', error);
-reply(`*AN ERROR OCCURRED!! MESSAGE :*\n\n> ${error.message}`);
-}
+    await conn.sendMessage(m.chat, { image: { url: apiUrl }, caption: `🎨 *Flux Image Generator*\n\n📄 *Prompt:* ${text}\n\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀᴠɪᴅ ᴄʏʀɪʟ ᴛᴇᴄʜ` }, { quoted: m });
+  } catch (error) {
+    console.error('Error in Flux command:', error);
+    reply(`*AN ERROR OCCURRED!! MESSAGE :*\n\n> ${error.message}`);
+  }
 });
