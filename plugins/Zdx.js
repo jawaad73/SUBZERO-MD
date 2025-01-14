71cd13d5acee9fc78a4d1830b3c4a9a67daa49ca
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-01-14 21:52:07 +0200

    Update Zdx.js

--- plugins/Zdx.js
+++ plugins/Zdx.js
@@ -1,4 +1,74 @@
-/*const config = require('../config');
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const { fetchJson } = require('../lib/functions');
+
+cmd({
+  pattern: 'play0',
+  desc: 'Play a song from YouTube',
+  category: 'music',
+  filename: __filename
+}, async (conn, mek, m, {
+  text,
+  from,
+  quoted,
+  body,
+  isCmd,
+  command,
+  args,
+  q,
+  isGroup,
+  sender,
+  senderNumber,
+  botNumber2,
+  botNumber,
+  pushname,
+  isMe,
+  isOwner,
+  groupMetadata,
+  groupName,
+  participants,
+  groupAdmins,
+  isBotAdmins,
+  isAdmins,
+  reply
+}) => {
+  try {
+    if (!text) {
+      return reply(`*Usage:* ${command} <song name>\n\n*Example:* ${command} Faded by Alan Walker`);
+    }
+
+    await reply('> *Processing...*');
+
+    const yts = require("yt-search");
+    let search = await yts(text);
+    let video = search.all[0];
+
+    let body = `*QUEEN_ANITA-V4_MUSIC - PLAYER*\n` + 
+    `> Title: *${video.title}*\n` + 
+    `> Views: *${video.views}*\n` + 
+    `> Duration: *${video.timestamp}*\n` + 
+    `> Uploaded: *${video.ago}*\n` + 
+    `> Url: *${video.url}*\n ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀᴠɪᴅ ᴄʏʀɪʟ ᴛᴇᴄʜ`;
+
+    await conn.sendMessage(m.chat, { image: { url: video.thumbnail }, caption: body }, { quoted: m });
+
+    const apiResponse = await axios.get(`https://davidcyril-api.onrender.com/convert`, { params: { url: video.url, format: 'mp3' } });
+
+    if (apiResponse.data.success) {
+      const { downloadUrl } = apiResponse.data;
+      await conn.sendMessage(m.chat, { audio: { url: downloadUrl }, mimetype: 'audio/mp4', fileName: `${video.title}.mp3`, caption: `🎧 *Here's your song:*\n> *Title:* ${video.title}` }, { quoted: m });
+    } else {
+      reply(`*Error fetching the song!*`);
+    }
+  } catch (error) {
+    console.error('Error during play command:', error);
+    reply(`*An error occurred while processing your request. Please try again later.*`);
+  }
+});
+
+
+
+/* const config = require('../config');
 const { cmd, commands } = require('../command');
 const { fetchJson } = require('../lib/functions');
 const yts = require("yt-search");
@@ -64,7 +134,7 @@ cmd({
     reply(`*An error occurred while processing your request. Please try again later.*`);
   }
 });
-*/
+
 
 /*//----+-++
 cmd({
