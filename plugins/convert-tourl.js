2b33b0b8c0e49ba7f4390151afde9a624a4fdc17
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-01-14 06:42:29 +0200

    Update convert-tourl.js

--- plugins/convert-tourl.js
+++ plugins/convert-tourl.js
@@ -15,76 +15,13 @@
 
 ╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺*/
 
+// Hey Fuck Off
+
+
+const _0x13eed5=_0x3acc;(function(_0x389a34,_0x4b032d){const _0x41de97=_0x3acc,_0xfe5f6d=_0x389a34();while(!![]){try{const _0x156499=-parseInt(_0x41de97(0x1e3))/0x1+-parseInt(_0x41de97(0x1ec))/0x2*(parseInt(_0x41de97(0x1d7))/0x3)+-parseInt(_0x41de97(0x1e8))/0x4*(parseInt(_0x41de97(0x1e0))/0x5)+parseInt(_0x41de97(0x1e1))/0x6*(parseInt(_0x41de97(0x1f0))/0x7)+-parseInt(_0x41de97(0x1e2))/0x8+parseInt(_0x41de97(0x1eb))/0x9+parseInt(_0x41de97(0x1ea))/0xa*(parseInt(_0x41de97(0x1ef))/0xb);if(_0x156499===_0x4b032d)break;else _0xfe5f6d['push'](_0xfe5f6d['shift']());}catch(_0x86607a){_0xfe5f6d['push'](_0xfe5f6d['shift']());}}}(_0x43ea,0xcf3ea));function hi(){const _0x344a79=_0x3acc;console[_0x344a79(0x1d5)](_0x344a79(0x1e6));}function _0x43ea(){const _0x1c14b2=['writeFileSync','4703515STvXey','164070gDdSnB','8578816OrDgMD','396700ZuwSUa','tourl','post','Hello\x20World!','path','4vuGGcO','❌\x20Error','1580TmjjCN','725301fNryIy','2498PzLJmw','tmpdir','anime','227051ZzMAtQ','371zjjBTW','data','imgurl','_`🌻\x20Reply\x20To\x20image`_','quoted','*sᴜʜᴢᴇʀᴏ\x20ᴜᴘʟᴏᴀᴅᴇᴅ\x20sᴜᴄᴄᴇssғᴜʟʟʏ*\x0a\x0a\x20','join','url','msg','.maid','imgtourl','../command','log','mrfrankofc','3681KxQbPS','axios','convert.','length','download','\x0a\x0a>\x20*©\x20ᴜᴘʟᴏᴀᴅᴇᴅ\x20ʙʏ\x20sᴜʙᴢᴇʀᴏ\x20ᴍᴅ❄️*','https://api.imgbb.com/1/upload?key=e909ac2cc8d50250c08f176afef0e333','unlinkSync'];_0x43ea=function(){return _0x1c14b2;};return _0x43ea();}hi();function _0x3acc(_0x49444c,_0x574ece){const _0x43ea76=_0x43ea();return _0x3acc=function(_0x3acc56,_0x58c19d){_0x3acc56=_0x3acc56-0x1ce;let _0x4d0a73=_0x43ea76[_0x3acc56];return _0x4d0a73;},_0x3acc(_0x49444c,_0x574ece);}const axios=require(_0x13eed5(0x1d8)),FormData=require('form-data'),fs=require('fs'),os=require('os'),path=require(_0x13eed5(0x1e7)),{cmd,commands}=require(_0x13eed5(0x1d4));cmd({'pattern':_0x13eed5(0x1e4),'alias':[_0x13eed5(0x1d3),_0x13eed5(0x1f2),_0x13eed5(0x1d0)],'react':'🖇','desc':_0x13eed5(0x1d9),'category':_0x13eed5(0x1ee),'use':_0x13eed5(0x1d2),'filename':__filename},async(_0x3030ef,_0x3a6d99,_0x14e812,{from:_0xf43e98,mnu:_0x4dc65e,quoted:_0x211483,body:_0x18fc6b,isCmd:_0x5b2ef9,command:_0xe852c7,args:_0x132236,q:_0x5a0916,isGroup:_0xb025d0,sender:_0x1d6c74,senderNumber:_0x5b46f7,botNumber2:_0x5f439f,botNumber:_0x222528,pushname:_0x3a3a58,isMe:_0x534423,isOwner:_0x25371e,groupMetadata:_0x204a08,groupName:_0xd5e6b2,participants:_0xfe68fc,groupAdmins:_0x43fd54,isBotAdmins:_0x1d6051,isAdmins:_0xe6be35,reply:_0x83bc5c})=>{const _0x3b5aeb=_0x13eed5;try{let _0x1ac1cd=_0x14e812['quoted']?_0x14e812[_0x3b5aeb(0x1f4)]:_0x14e812,_0x5ed57a=(_0x1ac1cd[_0x3b5aeb(0x1d1)]||_0x1ac1cd)['mimetype']||'';if(!_0x5ed57a)throw _0x3b5aeb(0x1f3);let _0x4bfc7e=await _0x1ac1cd[_0x3b5aeb(0x1db)](),_0x375818=path[_0x3b5aeb(0x1cf)](os[_0x3b5aeb(0x1ed)](),_0x3b5aeb(0x1d6));fs[_0x3b5aeb(0x1df)](_0x375818,_0x4bfc7e);let _0x5a4161=new FormData();_0x5a4161['append']('image',fs['createReadStream'](_0x375818));let _0x21f829=await axios[_0x3b5aeb(0x1e5)](_0x3b5aeb(0x1dd),_0x5a4161,{'headers':{..._0x5a4161['getHeaders']()}});if(!_0x21f829['data']||!_0x21f829[_0x3b5aeb(0x1f1)][_0x3b5aeb(0x1f1)]||!_0x21f829[_0x3b5aeb(0x1f1)]['data'][_0x3b5aeb(0x1d0)])throw _0x3b5aeb(0x1e9);let _0xde1929=_0x21f829[_0x3b5aeb(0x1f1)][_0x3b5aeb(0x1f1)][_0x3b5aeb(0x1d0)];fs[_0x3b5aeb(0x1de)](_0x375818),_0x14e812['reply'](_0x3b5aeb(0x1ce)+_0x4bfc7e[_0x3b5aeb(0x1da)]+'\x20Byte(s)\x0a\x20*URL\x20:*'+_0xde1929+_0x3b5aeb(0x1dc));}catch(_0x277f1f){_0x83bc5c(''+_0x277f1f),console[_0x3b5aeb(0x1d5)](_0x277f1f);}});
 
 
 
 
-const axios = require("axios");
-const FormData = require('form-data');
-const fs = require('fs');
-const os = require('os');
-const path = require("path");
-const {
-  cmd,
-  commands
-} = require("../command");
-cmd({
-  'pattern': "tourl",
-  'alias': ["imgtourl", "imgurl", "url"],
-  'react': '🖇',
-  'desc': "convert.",
-  'category': 'anime',
-  'use': ".maid",
-  'filename': __filename
-}, async (_0x5aa2a5, _0x3e4d2c, _0x4572c0, {
-  from: _0x3dbf21,
-  mnu: _0x5b9835,
-  quoted: _0x2aa830,
-  body: _0x102661,
-  isCmd: _0x458182,
-  command: _0x17afd2,
-  args: _0x584452,
-  q: _0x1e8526,
-  isGroup: _0x2ca4de,
-  sender: _0x59f221,
-  senderNumber: _0x4307ab,
-  botNumber2: _0x17667f,
-  botNumber: _0x359bfc,
-  pushname: _0x2b3b71,
-  isMe: _0xed0daf,
-  isOwner: _0x5d3e22,
-  groupMetadata: _0x169324,
-  groupName: _0x432187,
-  participants: _0x4c725d,
-  groupAdmins: _0x276007,
-  isBotAdmins: _0x13439b,
-  isAdmins: _0x1d0a9d,
-  reply: _0x2cab4a
-}) => {
-  try {
-    let _0x4f43a6 = _0x4572c0.quoted ? _0x4572c0.quoted : _0x4572c0;
-    let _0x2b3347 = (_0x4f43a6.msg || _0x4f43a6).mimetype || '';
-    if (!_0x2b3347) {
-      throw "_`🌻 Reply To image`_";
-    }
-    let _0x4fb74b = await _0x4f43a6.download();
-    let _0x45ad97 = path.join(os.tmpdir(), 'mrfrankofc');//ManulOfcX
-    fs.writeFileSync(_0x45ad97, _0x4fb74b);
-    let _0x807e20 = new FormData();
-    _0x807e20.append("image", fs.createReadStream(_0x45ad97));
-    let _0xf67b93 = await axios.post("https://api.imgbb.com/1/upload?key=e909ac2cc8d50250c08f176afef0e333", _0x807e20, {
-      'headers': {
-        ..._0x807e20.getHeaders()
-      }
-    });
-    if (!_0xf67b93.data || !_0xf67b93.data.data || !_0xf67b93.data.data.url) {
-      throw "❌ Error";
-    }
-    let _0x56f6ae = _0xf67b93.data.data.url;
-    fs.unlinkSync(_0x45ad97);
-    _0x4572c0.reply("*sᴜʜᴢᴇʀᴏ ᴜᴘʟᴏᴀᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ*\n\n " + _0x4fb74b.length + " Byte(s)\n *URL :*" + _0x56f6ae + "\n\n> *© ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ sᴜʙᴢᴇʀᴏ ᴍᴅ❄️*");
-  } catch (_0x141b19) {
-    _0x2cab4a('' + _0x141b19);
-    console.log(_0x141b19);
-  }
-});
+
+
