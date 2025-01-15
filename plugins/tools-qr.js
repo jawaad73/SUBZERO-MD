d4fa73fcedfceec71bd1446f893699e215e57209
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-01-15 16:35:29 +0200

    Update tools-qr.js

--- plugins/tools-qr.js
+++ plugins/tools-qr.js
@@ -36,7 +36,7 @@ cmd({
 }) => {
   try {
     if (!q) return reply('Please provide text to generate QR code.');
-    await reply('> *Generating QR code...*');
+    await reply('> *Subzero Generating QR code...🧩*');
     const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(q)}&size=200x200`;
     const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
     const buffer = Buffer.from(response.data, 'binary');
